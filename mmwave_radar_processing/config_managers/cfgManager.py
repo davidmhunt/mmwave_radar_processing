06518e1b556b2f3a6b441dac277abec8a7464d28
David Hunt <dmh89@duke.edu>
2024-08-26 19:34:36 -0400

    adding additional files to test system out

--- mmwave_radar_processing/config_managers/cfgManager.py
+++ mmwave_radar_processing/config_managers/cfgManager.py
@@ -26,7 +26,7 @@ class ConfigManager:
         self.adcCfg_adcOutputFmt:int = 0
 
         #adcbufCfg
-        self.adcbufCfg_sub_frame_idx:int = 0
+        # self.adcbufCfg_sub_frame_idx:int = 0
         self.adcbufCfg_adc_output_fmt:int = 0
         self.adcbufCfg_sample_swap:bool = False
         self.adcbufCfg_channel_interleave:bool = False
@@ -89,7 +89,6 @@ class ConfigManager:
         self.num_tx_antennas:int = 3
         self.num_rx_antennas:int = 4
         self.virtual_antennas_enabled:bool = False
-        self.angle_bins:np.ndarray = np.empty(shape=0,dtype=float)
 
         # status flags
         self.config_loaded = False
@@ -100,9 +99,13 @@ class ConfigManager:
     #Computing radar performance
     ####################################################################
 
-    def compute_radar_perforance(self):
+    def compute_radar_perforance(self,profile_idx:int=0):
+
+        self._compute_range_performance(profile_idx)
+        self._compute_vel_performance(profile_idx)
+        self._compute_angular_performance()
         
-        pass
+        
 
     def _compute_range_performance(self,profile_idx:int = 0):
 
@@ -122,20 +125,44 @@ class ConfigManager:
         
         self.range_max_m = (constants.c * self.get_adc_sample_rate_kSps(profile_idx) * 1e3) / \
                 (2 * self.get_chirp_slope_MHz_us(profile_idx) * (1e6/1e-6))
-        
-        self.range_bins_m = np.arange(
-            start=0,
-            stop=self.range_max_m,
-            step=self.range_bin_size_m
-        )
 
         return
     
-    def compute_vel_performance(self):
+    def _compute_vel_performance(self,profile_idx:int = 0):
+        
 
-        pass
+        # get required quantities to compute velocity performance
+        start_freq_GHz = float(self.profile_cfgs[profile_idx]["startFreq_GHz"])
+        lambda_m = constants.c / (start_freq_GHz * 1e9)
+        chirps_per_loop = self.frameCfg_end_index - self.frameCfg_start_index + 1
+        chirp_loops_per_frame = float(self.frameCfg_loops)
+        num_chirps_per_frame = int(
+            (self.frameCfg_end_index - self.frameCfg_start_index + 1)
+            * chirp_loops_per_frame
+        )
+        ramp_end_time_us = float(self.profile_cfgs[profile_idx]["rampEndTime_us"])
+        idle_time_us = float(self.profile_cfgs[profile_idx]["idleTime_us"])
+        chirp_period_us = ramp_end_time_us + idle_time_us
 
-    def compute_angular_performance(self):
+        # velocity_resolution_m_per_s
+        self.vel_res_m_s = lambda_m / (
+            2 * chirp_period_us * chirps_per_loop * 1e-6 * num_chirps_per_frame
+        )
+
+        # max_velocity
+        self.vel_max_m_s = lambda_m / (4 * chirp_period_us * chirps_per_loop * 1e-6)
+        
+        return
+
+    def _compute_angular_performance(self):
+
+        chirps_per_loop = self.frameCfg_end_index - self.frameCfg_start_index + 1
+
+        if chirps_per_loop > 1:
+            self.virtual_antennas_enabled = True
+        else:
+            self.virtual_antennas_enabled=False
+
         pass
 
     ####################################################################
@@ -168,6 +195,8 @@ class ConfigManager:
         
         self.config_loaded = True
 
+        self.compute_radar_perforance(profile_idx=0)
+
 
     ####################################################################
     #loading radar configuration from .cfg file
@@ -187,11 +216,11 @@ class ConfigManager:
             case "adcbufCfg":
                 self._load_adcbufCfg_from_cfg(str_split)
             case "profileCfg":
-                pass
+                self._load_profileCfg_from_cfg(str_split)
             case "chirpCfg":
-                pass
+                self._load_chirpCfg_from_cfg(str_split)
             case "frameCfg":
-                pass
+                self._load_frame_cfg_from_cfg(str_split)
     
     def _load_channelCfg_from_cfg(self, params: list):
         
@@ -223,11 +252,11 @@ class ConfigManager:
     
     def _load_adcbufCfg_from_cfg(self, params: list):
 
-        self.adcbufCfg_sub_frame_idx = int(params[1])
-        self.adcbufCfg_adc_output_fmt = int(params[2]) #0 for complex, 1 for real
-        self.adcbufCfg_sample_swap = False if int(params[3]) == 0 else True
-        self.adcbufCfg_channel_interleave = True if int(params[4]) == 0 else False
-        self.adcbufCfg_chirp_threshold = int(params[5])
+        # self.adcbufCfg_sub_frame_idx = int(params[1])
+        self.adcbufCfg_adc_output_fmt = int(params[-4]) #0 for complex, 1 for real
+        self.adcbufCfg_sample_swap = False if int(params[-3]) == 0 else True
+        self.adcbufCfg_channel_interleave = True if int(params[-2]) == 0 else False
+        self.adcbufCfg_chirp_threshold = int(params[-1])
 
         return
 
@@ -250,7 +279,7 @@ class ConfigManager:
             "rxGain_dB": float(params[14]),
         }
 
-        if self.profile_cfgs[0]["profileID"] == -1:
+        if self.profile_cfgs[0]["profileId"] == -1:
             self.profile_cfgs[0] = new_profile
         elif new_profile["profileId"] < len(self.profile_cfgs):
             print("cfgManager: attempted to load multiple profiles with the same ID")
