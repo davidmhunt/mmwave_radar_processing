Fix range and velocity bin calculations to prevent off-by-one errors

mmwave_radar_processing/processors/range_angle_resp.py
@@ -30,7 +30,7 @@ class RangeAngleProcessor(_Processor):
         self.range_bins = np.arange(
             start=0,
             step=self.config_manager.range_res_m,
-            stop=self.config_manager.range_max_m - self.config_manager.range_res_m/2)
+            stop=self.config_manager.range_max_m - self.config_manager.range_res_m/2) + 1e-3
 
         #compute the phase shifts
         self.num_rx_antennas = self.config_manager.num_rx_antennas

mmwave_radar_processing/processors/range_doppler_resp.py
@@ -23,7 +23,7 @@ class RangeDopplerProcessor(_Processor):
         
         self.vel_bins = np.arange(
             start=-1 * self.config_manager.vel_max_m_s,
-            stop = self.config_manager.vel_max_m_s - self.config_manager.vel_res_m_s,
+            stop = self.config_manager.vel_max_m_s - self.config_manager.vel_res_m_s + 1e-3,
             step= self.config_manager.vel_res_m_s
         )
 
@@ -31,7 +31,7 @@ class RangeDopplerProcessor(_Processor):
         self.range_bins = np.arange(
             start=0,
             step=self.config_manager.range_res_m,
-            stop=self.config_manager.range_max_m - self.config_manager.range_res_m/2)
+            stop=self.config_manager.range_max_m - self.config_manager.range_res_m/2 + 1e-3)
     
     def apply_range_vel_hanning_window(self,
             adc_cube: np.ndarray):